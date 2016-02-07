ea9e8c9cff45f7f71b1abec026ab11e58b555d18
zeroRooter <williamap@gmail.com>
2016-02-06 19:30:31 -0500

    add babel, start speration of concerns

---
+++ src/modules/tasklist.js
@@ -0,0 +1,14 @@
+'use strict'
+/**
+  * Windows tasklist nodejs module
+  * Get a list of Pids of the processes that are running
+  * @param {string} sProcessName? - Filter. If provided, only return the Pids of processes running with this name
+  * @return {array} aTasks - an array strings containing the Pids of the running processes or of the processes with the name sProcessName
+*/
+const tasklist = (sProcessName) => {
+  const child_process = require('child_process');
+  let oBufferTasks = child_process.execSync('tasklist /FI "IMAGENAME eq chrome.exe"', {}, (error, stdout, stderr) => {sTasks = stdout});
+  return oBufferTasks.toString();
+}
+
+export {tasklist};
